ac202d1e5990a6cdd01a415e3da442a500398df4
André Bjärby <andre.bjarby@nateko.lu.se>
2018-06-11 09:57:33 +0200

    Remove the stilt run directory once the results are retrieved.

--- stiltcluster/src/main/scala/se/lu/nateko/cp/stiltcluster/WorkMaster.scala
+++ stiltcluster/src/main/scala/se/lu/nateko/cp/stiltcluster/WorkMaster.scala
@@ -5,6 +5,7 @@ import java.nio.file.{Files, Paths}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.sys.process._
 
 import akka.actor.{Actor, ActorIdentity, ActorRef, Identify, Props, Terminated}
 
@@ -111,6 +112,8 @@ class WorkMaster(nCores: Int, prodAddr: String) extends Trace with TrackSlotProd
 
 			finishSlot(SlotCalculated(StiltResult(slot, d.resolve("output"))))
 
+			s"rm -rf -- ${d}".!!
+			trace(s"Removed the ${d} directory")
 			trace(s"Slot calculation for $slot was a success")
 		}
 
