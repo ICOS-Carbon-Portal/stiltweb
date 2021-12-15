a3a91d5c90381c0c90e8a2e8d87fbe24d46f189a
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-12-15 14:12:40 +0100

    [scala3] A taste of optional-braces syntax

--- stiltcluster/src/main/scala/se/lu/nateko/cp/stiltcluster/LogLineQueue.scala
+++ stiltcluster/src/main/scala/se/lu/nateko/cp/stiltcluster/LogLineQueue.scala
@@ -4,35 +4,31 @@ import scala.collection.mutable.Queue
 import scala.collection.immutable.Seq
 import scala.annotation.tailrec
 
-class LogLineQueue(sizeBound: Int){
+class LogLineQueue(sizeBound: Int):
 
 	private val queue = Queue.empty[String]
 	private var remainder = Array.empty[Byte]
 
-	def append(bytes: Array[Byte]): Unit = {
+	def append(bytes: Array[Byte]): Unit =
 		remainder ++= bytes
 		enqueueCompleteLines()
-	}
 
-	def flush(): Unit = {
+	def flush(): Unit =
 		enqueue(remainder)
 		remainder = Array.empty[Byte]
-	}
 
 	def lines: Seq[String] = queue.toVector
 
-	@tailrec private def enqueueCompleteLines(): Unit = {
+	@tailrec private def enqueueCompleteLines(): Unit =
 		val nlPos = remainder.indexOf(10)
 		if(nlPos >= 0){
 			enqueue(remainder.take(nlPos + 1))
 			remainder = remainder.drop(nlPos + 1)
 			enqueueCompleteLines()
 		}
-	}
 
-	private def enqueue(bytes: Array[Byte]): Unit = {
+	private def enqueue(bytes: Array[Byte]): Unit =
 		queue += new String(bytes, "UTF-8").stripLineEnd
 		if(queue.size > sizeBound) queue.dequeue()
-	}
 
-}
+end LogLineQueue
