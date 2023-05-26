fccfcdc797e65a604112a5be7e8955787885a51c
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2023-05-26 18:13:50 +0200

    [atmoaccess] Preparing for ATMO-ACCESS login

--- src/main/scala/se/lu/nateko/cp/stiltweb/AuthRouting.scala
+++ src/main/scala/se/lu/nateko/cp/stiltweb/AuthRouting.scala
@@ -1,38 +1,42 @@
 package se.lu.nateko.cp.stiltweb
 
 
-import se.lu.nateko.cp.cpauth.core.PublicAuthConfig
-import akka.http.scaladsl.server.Directives._
-import se.lu.nateko.cp.cpauth.core.Authenticator
-import akka.http.scaladsl.server.Route
-import se.lu.nateko.cp.cpauth.core.UserId
-import scala.util.Success
-import scala.util.Failure
-import se.lu.nateko.cp.cpauth.core.CookieToToken
 import akka.http.javadsl.server.CustomRejection
+import akka.http.scaladsl.server.Directive1
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.server.Route
+import se.lu.nateko.cp.cpauth.core.AuthSource
+import se.lu.nateko.cp.cpauth.core.AuthToken
+import se.lu.nateko.cp.cpauth.core.Authenticator
+import se.lu.nateko.cp.cpauth.core.CookieToToken
+import se.lu.nateko.cp.cpauth.core.PublicAuthConfig
+import se.lu.nateko.cp.cpauth.core.UserId
 
-class AuthRouting(authConfig: PublicAuthConfig) {
+import scala.util.Failure
+import scala.util.Success
+
+class AuthRouting(authConfig: PublicAuthConfig):
 
 	private[this] val authenticator = Authenticator(authConfig).get
 
-	def user(inner: UserId => Route): Route = cookie(authConfig.authCookieName)(cookie => {
-		val tokenTry = for(
-			signedToken <- CookieToToken.recoverToken(cookie.value);
-			token <- authenticator.unwrapToken(signedToken)
-		) yield token
-
-		tokenTry match {
+	val authToken: Directive1[AuthToken] = cookie(authConfig.authCookieName).flatMap{cookie =>
+		CookieToToken.recoverToken(cookie.value).flatMap(authenticator.unwrapToken) match
 			case Success(token) =>
-				inner(token.userId)
+				provide(token)
 			case Failure(err) =>
-				reject(new CpauthAuthenticationFailedRejection(toMessage(err)))
-		}
-	})
+				reject(CpauthAuthFailedRejection("Authentication cookie invalid or absent: " + toMessage(err)))
+	}
 
-	private def toMessage(err: Throwable): String = {
+	val user: Directive1[UserId] = authToken.flatMap{token =>
+		if token.source != AuthSource.AtmoAccess then
+			reject(CpauthAuthFailedRejection("User did not log in through ATMO ACCESS"))
+		else provide(token.userId)
+	}
+
+	private def toMessage(err: Throwable): String =
 		val msg = err.getMessage
 		if(msg == null || msg.isEmpty) err.getClass.getName else msg
-	}
-}
 
-class CpauthAuthenticationFailedRejection(val msg: String) extends CustomRejection
+end AuthRouting
+
+class CpauthAuthFailedRejection(val msg: String) extends CustomRejection
