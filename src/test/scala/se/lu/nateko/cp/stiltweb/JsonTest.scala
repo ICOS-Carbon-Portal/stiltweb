f363b11093f81058dfe93617f18b9b3b8951f1e2
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2024-03-22 17:45:20 +0100

    [worker] Fix broken test

--- src/test/scala/se/lu/nateko/cp/stiltweb/JsonTest.scala
+++ src/test/scala/se/lu/nateko/cp/stiltweb/JsonTest.scala
@@ -4,42 +4,18 @@ import java.time.LocalDate
 
 import se.lu.nateko.cp.stiltcluster.Job
 
-import spray.json._
+import spray.json.*
 import se.lu.nateko.cp.stiltweb.marshalling.StiltJsonSupport.{given RootJsonFormat[Job]}
 import org.scalatest.funsuite.AnyFunSuite
 
-class JsonTest extends AnyFunSuite {
-
-	// spray-json is way up in the cloud, this test brings it down to the ground
-	// so that one can get a feel for what's happening.
+class JsonTest extends AnyFunSuite:
 	test("Serialize and deserialize JSON") {
 
 		val job = Job("HTM", 56.10, 13.42, 150,
 					  LocalDate.of(2012, 1, 1),
 					  LocalDate.of(2012, 1, 2),
-					  "user")
+					  "user").submittedNow
 
-		// This is the string the toJson returned for me.
-		//   {"stop":"2012-01-02","id":"job_1438388676","alt":150,"lon":13.42,
-		//    "siteId":"HTM","userId":"user","start":"2012-01-01","lat":56.1}
-		// Since the order of the keys can vary, we convert it to lines and sort
-		// them before testing.
-		val j = job.toJson
-		val s = j.toString
-		val t = s.slice(1, s.length-1).split(",").map(_.trim).sorted.mkString("\n")
-
-		// This particular test checks that the timeStarted and timeStopped
-		// fields are not being serialized.
-		assert(t == """"alt":150
-					   |"id":"job_940257578"
-					   |"lat":56.1
-					   |"lon":13.42
-					   |"siteId":"HTM"
-					   |"start":"2012-01-01"
-					   |"stop":"2012-01-02"
-					   |"userId":"user"""".stripMargin)
-
-		val job2 = j.convertTo[Job]
+		val job2 = job.toJson.compactPrint.parseJson.convertTo[Job]
 		assert(job == job2)
 	}
-}
