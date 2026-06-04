df2b1a0a569affbafb3ef30d3483a49c83c6c080
Andrew Debevec <andrew.debevec@nateko.lu.se>
2026-06-04 13:58:49 +0200

    Revert "[atmoaccess] Preparing for ATMO-ACCESS login"
    This reverts commit fccfcdc797e65a604112a5be7e8955787885a51c.

--- src/main/scala/se/lu/nateko/cp/stiltweb/AuthRouting.scala
+++ src/main/scala/se/lu/nateko/cp/stiltweb/AuthRouting.scala
@@ -1,42 +1,37 @@
 package se.lu.nateko.cp.stiltweb
 
-
-import akka.http.javadsl.server.CustomRejection
-import akka.http.scaladsl.server.Directive1
-import akka.http.scaladsl.server.Directives.*
-import se.lu.nateko.cp.cpauth.core.AuthSource
-import se.lu.nateko.cp.cpauth.core.AuthToken
-import se.lu.nateko.cp.cpauth.core.Authenticator
-import se.lu.nateko.cp.cpauth.core.CookieToToken
 import se.lu.nateko.cp.cpauth.core.PublicAuthConfig
+import akka.http.scaladsl.server.Directives._
+import se.lu.nateko.cp.cpauth.core.Authenticator
+import akka.http.scaladsl.server.Route
 import se.lu.nateko.cp.cpauth.core.UserId
-
-import scala.util.Failure
 import scala.util.Success
-import akka.http.scaladsl.model.StatusCodes
+import scala.util.Failure
+import se.lu.nateko.cp.cpauth.core.CookieToToken
+import akka.http.javadsl.server.CustomRejection
 
-class AuthRouting(authConfig: PublicAuthConfig):
+class AuthRouting(authConfig: PublicAuthConfig) {
 
 	private[this] val authenticator = Authenticator(authConfig).get
 
-	val authToken: Directive1[AuthToken] = cookie(authConfig.authCookieName).flatMap{cookie =>
-		CookieToToken.recoverToken(cookie.value).flatMap(authenticator.unwrapToken) match
+	def user(inner: UserId => Route): Route = cookie(authConfig.authCookieName)(cookie => {
+		val tokenTry = for(
+			signedToken <- CookieToToken.recoverToken(cookie.value);
+			token <- authenticator.unwrapToken(signedToken)
+		) yield token
+
+		tokenTry match {
 			case Success(token) =>
-				provide(token)
+				inner(token.userId)
 			case Failure(err) =>
-				reject(CpauthAuthFailedRejection("Authentication cookie invalid or absent: " + toMessage(err)))
-	}
+				reject(new CpauthAuthenticationFailedRejection(toMessage(err)))
+		}
+	})
 
-	val user: Directive1[UserId] = authToken.flatMap{token =>
-		if token.source != AuthSource.AtmoAccess then
-			reject(CpauthAuthFailedRejection("User did not log in through ATMO ACCESS"))
-		else provide(token.userId)
-	}
-
-	private def toMessage(err: Throwable): String =
+	private def toMessage(err: Throwable): String = {
 		val msg = err.getMessage
 		if(msg == null || msg.isEmpty) err.getClass.getName else msg
+	}
+}
 
-end AuthRouting
-
-class CpauthAuthFailedRejection(val msg: String) extends CustomRejection
+class CpauthAuthenticationFailedRejection(val msg: String) extends CustomRejection
